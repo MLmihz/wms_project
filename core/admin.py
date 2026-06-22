28bfcab1192d38db5b1fa88d7e2e99960aefea89
Michael Udoye <michael.udoye@strathmore.edu>
2026-06-22 18:01:08 +0300

    provider-admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import (
+    Resident,
+    WasteServiceProvider,
+    Administrator,
+    WasteReport,
+    CollectionSchedule,
+    RecyclingGuide,
+    Notification,
+    SystemLog,
+)
 
-# Register your models here.
+admin.site.register(Resident)
+admin.site.register(WasteServiceProvider)
+admin.site.register(Administrator)
+admin.site.register(WasteReport)
+admin.site.register(CollectionSchedule)
+admin.site.register(RecyclingGuide)
+admin.site.register(Notification)
+admin.site.register(SystemLog)
